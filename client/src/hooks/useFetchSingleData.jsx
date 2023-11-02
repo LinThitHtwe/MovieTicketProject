ffc3292c0cca19725c32707502ff2263c808b5fe
LinThit27 <linthit2745@gmail.com>
2023-11-03 00:47:31 +0630

    reuseable single useFetchSingleData and movie Detial Fetch

--- client/src/hooks/useFetchSingleData.jsx
+++ client/src/hooks/useFetchSingleData.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import axios from "axios";
 import { useQuery } from "react-query";
 
-const UseFetchSingleData = (queryKey) => {
-  return useQuery(queryKey);
+const fetchSingleData = async (fetchUrl) => {
+  const response = await axios.get(fetchUrl);
+  return response.data;
+};
+
+const UseFetchSingleData = (queryKey, fetchUrl) => {
+  return useQuery(queryKey, () => fetchSingleData(fetchUrl));
 };
 
 export default UseFetchSingleData;
--- client/src/hooks/useListData.jsx
+++ client/src/hooks/useListData.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 //import { fetchMovies } from "../query-functions/movieQueryFunction";
-const fetchMovies = async (fetchUrl) => {
+const fetchDatas = async (fetchUrl) => {
   const response = await axios.get(fetchUrl);
   return response.data;
 };
 const UseListData = (queryKey, fetchUrl) => {
-  return useQuery(queryKey, () => fetchMovies(fetchUrl));
+  return useQuery(queryKey, () => fetchDatas(fetchUrl));
 };
 
 export default UseListData;
