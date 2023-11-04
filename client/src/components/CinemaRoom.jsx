cac1205a2166ffdf4a4fb59204634af63b5362d8
Lin Thit <linthit2745@gmail.com>
2023-11-04 15:16:49 +0630

    added loading skeleton for cinema card and cinema room

--- client/src/components/CinemaRoom.jsx
+++ client/src/components/CinemaRoom.jsx
@@ -5,9 +5,24 @@ import { Link } from "react-router-dom";
 const CinemaRoom = ({ cinemaId }) => {
   const queryKey = ["cinema-room", cinemaId];
   const fetchUrl = `/Tbl_CinemaRoom?CinemaId=${cinemaId}`;
-  const { data } = UseFetchData(queryKey, fetchUrl);
+  const { isLoading, data } = UseFetchData(queryKey, fetchUrl);
   return (
     <>
+      {isLoading && (
+        <div className="pt-4 mt-4 flex gap-4 flex-wrap">
+          {Array(4)
+            .fill()
+            .map((_, index) => (
+              <button
+                key={index}
+                className="p-2 ml-2 bg-gray-800 rounded-2xl shadow-custom shadow-slate-700
+                 hover:bg-gray-700 hover:scale-105 transition-all duration-300 ease-in-out inline-block "
+              >
+                Loading...
+              </button>
+            ))}
+        </div>
+      )}
       <div className="pt-4 mt-4 flex gap-4 flex-wrap">
         {data &&
           data.map((d) => (
